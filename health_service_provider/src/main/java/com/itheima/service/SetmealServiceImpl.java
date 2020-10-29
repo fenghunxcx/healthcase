850d31d1ad5814476b96e388bac0fffb789baeef
fjs <840230450@qq.com>
2020-10-29 15:43:01 +0800

    修改-在新增、修改、删除套餐时删除redis&测试

--- health_service_provider/src/main/java/com/itheima/service/SetmealServiceImpl.java
+++ health_service_provider/src/main/java/com/itheima/service/SetmealServiceImpl.java
@@ -1,6 +1,8 @@
 package com.itheima.service;
 
 import com.alibaba.dubbo.config.annotation.Service;
+import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.JSONArray;
 import com.github.pagehelper.Page;
 import com.github.pagehelper.PageHelper;
 import com.itheima.constant.RedisConst;
@@ -12,7 +14,7 @@ import com.itheima.pojo.Setmeal;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.transaction.annotation.Transactional;
 import redis.clients.jedis.JedisPool;
-
+import redis.clients.jedis.Jedis;
 import java.util.List;
 import java.util.Map;
 
@@ -25,7 +27,7 @@ import java.util.Map;
 public class SetmealServiceImpl implements SetmealService {
     @Autowired
     SetmealDao setmealDao;
-
+    private final String REDIS_SETMEAL = "Setmeal";
     @Autowired
     JedisPool jedisPool;
 
@@ -36,8 +38,11 @@ public class SetmealServiceImpl implements SetmealService {
         setmealDao.add(setmeal);
         //2. 维护套餐和检查组的关系
         setRelation(setmeal, checkgroupIds);
-
-        jedisPool.getResource().srem(RedisConst.SETMEAL_PIC_RESOURCES, setmeal.getImg());
+        //删除redis
+        deletJedis(REDIS_SETMEAL);
+        if (setmeal.getImg() != null) {
+            jedisPool.getResource().srem(RedisConst.SETMEAL_PIC_RESOURCES, setmeal.getImg());
+        }
 
     }
 
@@ -61,14 +66,36 @@ public class SetmealServiceImpl implements SetmealService {
         return new PageResult(pageSetmeal.getTotal(),pageSetmeal.getResult());
     }
 
+    //移动端查询所有套餐
     @Override
     public List<Setmeal> findAll() {
-        return setmealDao.findAll();
+        Jedis jedis = jedisPool.getResource();
+        String redisSetmeal = jedis.get(REDIS_SETMEAL);
+        //JSON字符串转java对象
+        List<Setmeal> setmealList = JSONArray.parseArray(redisSetmeal, Setmeal.class);
+        //redis没有从数据库查
+        if (redisSetmeal == null) {
+            setmealList = setmealDao.findAll();
+            addJedis(setmealList);
+
+        }
+        return setmealList;
+
     }
 
     @Override
     public Setmeal findDetailById(Integer id) {
-        return setmealDao.findDetailsById(id);
+        Jedis jedis = jedisPool.getResource();
+        String s1 = jedis.get(id + "");
+        Setmeal setmeal = JSONArray.parseObject(s1, Setmeal.class);
+        if (s1 == null) {
+            setmeal = setmealDao.findDetailsById(id);
+            String s = JSON.toJSONString(setmeal);
+            jedis.set(id+"",s);
+
+        }
+
+        return setmeal;
     }
 
     @Override
@@ -84,7 +111,11 @@ public class SetmealServiceImpl implements SetmealService {
 
     @Override
     public void edit(Setmeal setmeal, Integer[] checkgroupIds) {
-        setmealDao.edit(setmeal);
+        long edit = setmealDao.edit(setmeal);
+        if (edit > 0) {
+            deletJedis(String.valueOf(setmeal.getId()));
+            deletJedis(REDIS_SETMEAL);
+        }
         Integer setmealId = setmeal.getId();
         if(setmealId != null){
             setmealDao.delRelation(setmealId);
@@ -100,11 +131,31 @@ public class SetmealServiceImpl implements SetmealService {
             throw new BusinessRuntimeException("删除失败,此套餐与订单关联");
         }
         setmealDao.delRelation(id);
-        setmealDao.delById(id);
+        long del = setmealDao.delById(id);
+        if (del > 0) {
+            deletJedis(String.valueOf(id));
+            deletJedis(REDIS_SETMEAL);
+        }
     }
 
     @Override
     public List<Integer> findCheckGroupIdsById(Integer id) {
         return setmealDao.findCheckGroupIdsById(id);
     }
+
+
+    private void addJedis(List<Setmeal> setmeal) {
+        Jedis jedis = jedisPool.getResource();
+        String s = JSON.toJSONString(setmeal);
+        jedis.set(REDIS_SETMEAL,s);
+    }
+    private void deletJedis(String key) {
+        Jedis jedis = jedisPool.getResource();
+        if (key != null) {
+            jedis.del(key);
+        }
+
+    }
+
+
 }
