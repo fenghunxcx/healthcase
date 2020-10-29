7330afac417f0c5e541c89d7c6016658f006e51a
liushan <liushan@qq.com>
2020-10-29 20:21:34 +0800

    新增：操作权限控制；

--- health_oms_backend/src/main/java/com/itheima/controller/PermissionConntroller.java
+++ health_oms_backend/src/main/java/com/itheima/controller/PermissionConntroller.java
@@ -1,9 +1,13 @@
 package com.itheima.controller;
 
 import com.alibaba.dubbo.config.annotation.Reference;
+import com.itheima.entity.PageResult;
+import com.itheima.entity.QueryPageBean;
 import com.itheima.entity.Result;
 import com.itheima.pojo.Permission;
 import com.itheima.service.PermissionService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -17,6 +21,7 @@ import java.util.List;
  */
 @RestController
 @RequestMapping("/permission")
+@Slf4j
 public class PermissionConntroller {
     @Reference
     PermissionService permissionService;
@@ -26,4 +31,54 @@ public class PermissionConntroller {
     List<Permission>  permissions = permissionService.findAll();
     return new Result(true,"查询操作权限成功",permissions);
     }
+
+    @RequestMapping("/findPage")
+    public Result findPage(@RequestBody QueryPageBean queryPageBean){
+        log.debug("controller{"+queryPageBean+"}");
+        PageResult pageResult = permissionService.findPage(queryPageBean);
+        log.debug("权限分页查询成功!!");
+        return new Result(true, "权限分页查询成功", pageResult);
+    }
+
+    @RequestMapping("/add")
+    public Result add(@RequestBody Permission permission){
+        log.debug("Permission~~~~~~~~~==================");
+        permissionService.add(permission);
+        log.debug("权限添加成功！！！");
+        return new Result(true,"权限添加成功");
+    }
+
+    @RequestMapping("/findById")
+    public Result findById(Integer id){
+        log.debug("findById~~~~~~~~~~~~~~~~"+id);
+        Permission permission=permissionService.findById(id);
+        log.debug("根据id查询成功~~~~~~~~~");
+        return new Result(true,"权限查询成功根据id",permission);
+    }
+
+
+    @RequestMapping("/edit")
+    public Result edit(@RequestBody Permission permission){
+        log.debug("permissionedit================");
+        permissionService.edit(permission);
+        log.debug("权限修改成功");
+        return new Result(true,"权限编辑成功");
+    }
+
+    @RequestMapping("/delById")
+    public Result delById(Integer id){
+        log.debug("删除----------controller");
+
+        permissionService.delById(id);
+
+        log.debug("删除权限成功-------=-=-=-=-");
+        return  new Result( true,"删除权限成功！！");
+    }
+
+
+
+
+
+
+
 }
